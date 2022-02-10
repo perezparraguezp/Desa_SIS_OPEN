95466249a7683f782fedd3feb22e85b070dd23c8
Pablo <perezparraguezp@gmail.com>
2022-02-09 21:40:48 -0300

    Formulario A012 - A

---
+++ modulo/rem/formulario/A02/A.php
@@ -0,0 +1,93 @@
+<?php
+include '../../../../php/config.php';
+include '../../../../php/objetos/persona.php';
+include '../../../../php/objetos/profesional.php';
+session_start();
+$rut = $_SESSION['rut'];
+$id_usuario = $_SESSION['id_usuario'];
+$profesional = new profesional($id_usuario);
+$tipo_profesional = $profesional->tipo_profesional;
+
+?>
+<script type="text/javascript">
+    $(function(){
+        $.post('formulario/base.php',{
+        },function(data){
+            $("#info_paciente").html(data);
+        });
+
+
+
+    })
+</script>
+<style type="text/css">
+    #formulario_final .container{
+        border: solid 1px rgba(204,204,221,0.86);
+        padding: 10px;
+        font-size: 0.7em;
+    }
+</style>
+<style type="text/css">
+
+    .settings-section
+    {
+
+        height: 45px;
+        width: 100%;
+
+    }
+
+    .settings-label
+    {
+        font-weight: bold;
+        font-family: Sans-Serif;
+        font-size: 14px;
+        margin-left: 14px;
+        margin-top: 15px;
+        float: left;
+    }
+
+    .settings-setter
+    {
+        float: right;
+        margin-right: 14px;
+        margin-top: 8px;
+    }
+</style>
+<div class="container" id="formulario_final" >
+    <div class="row">
+        <div class="col l5">
+            <div class="container eh-open_fondo" id="info_paciente">
+                <strong>ATENCIÓN POR PROFESIONAL</strong>
+                <p>asasasa sa sa sakaksjkj kskjsa.</p>
+            </div>
+        </div>
+        <div class="col l7">
+            <div class="container" id="info_evaluacion">
+                <div class="row">
+                    <div class="col l12">
+                        <header>INFORMACIÓN</header>
+                        <p>Se realizara un registro en relación al perfil del profesional. <br />Tipo Profesional: <strong><?php echo $tipo_profesional; ?></strong><br />Nombre Profesional: <strong><?php echo $profesional->nombre; ?></strong></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col l6">
+                        <input type="button"
+                               style="width: 100%;"
+                               onclick="loadMenu_REM('menu_2','registro_atencion','')"
+                               class="btn-large red lighten-2 white-text"
+                               value="CANCELAR" />
+                    </div>
+                    <div class="col l6">
+                        <input type="button"
+                               style="width: 100%;"
+                               onclick="insertRegistro()"
+                               class="btn-large eh-open_principal"
+                               value="GUARDAR" />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
