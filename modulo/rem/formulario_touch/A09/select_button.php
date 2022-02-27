893d1a710a6b2f5a673e2ddc04ab2c824202b075
Pablo Perez <perezparraguezp@gmail.com>
2022-02-27 16:02:03 -0300

    COMIENZO MENU A09

---
+++ modulo/rem/formulario_touch/A09/select_button.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <div id="A2" class="card col l2 eh-open_principal card_cuadrada" style="font-size: 0.7em;padding-top: 10px;margin-left: 5px;"
+         onclick="loadFormulario_touch('A')">
+        CONSULTAS Y CONTROLES ODONTOLÓGICOS REALIZADOS EN APS
+    </div>
+    <div id="LLAMADAS" class="card col l2 eh-open_principal card_cuadrada" style="font-size: 0.7em;padding-top: 10px;margin-left: 5px;"
+         onclick="loadFormulario_touch('LLAMADAS')">
+        REGISTRO DE LLAMADAS
+    </div>
+</div>
